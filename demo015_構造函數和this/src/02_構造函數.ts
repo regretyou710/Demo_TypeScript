362ccde43a487070b00f78ac53ca7c65b707ada0
eason <regretyou710@gmail.com>
2023-06-04 12:45:39 +0800

    2023060412445

---
+++ demo015_構造函數和this/src/02_構造函數.ts
@@ -0,0 +1,25 @@
+class Dog {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    // 在實例方法中，this就表示當前的實例
+    // 在構造函數中當前對象就是當前新建的那個對象
+    // 可以透過this向新建的對象中添加屬性
+    // console.log(this);
+    this.name = name;
+    this.age = age;
+  }
+
+  bark() {
+    // 在方法中可以透過this來表示當前調用方法的對象
+    console.log(this.name);
+  }
+}
+
+const dog1 = new Dog("小黑", 3);
+const dog2 = new Dog("小白", 1);
+console.log(dog1);
+console.log(dog2);
+dog1.bark();
+dog2.bark();
