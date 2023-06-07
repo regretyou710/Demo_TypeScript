d1a205adc061be9086ba9433109d55a4d1eb8679
eason <regretyou710@gmail.com>
2023-06-07 19:12:35 +0800

    202306071912

---
+++ demo020_屬性的封裝/dist/04_super關鍵字.js
@@ -0,0 +1,24 @@
+"use strict";
+(function () {
+    class Animal {
+        constructor(name) {
+            this.name = name;
+        }
+        speak() {
+            console.log("動物叫");
+        }
+    }
+    class Dog extends Animal {
+        constructor(name, age) {
+            super(name);
+            this.age = age;
+        }
+        speak() {
+            // super.speak();
+            console.log("汪汪叫");
+        }
+    }
+    const dog = new Dog("小狗", 3);
+    console.log(dog);
+    dog.speak();
+})();
